b253769ff26aef4bdc547719613a4ed86a81d305
Gal Schlezinger <gal@spitfire.co.il>
2024-07-25 09:48:46 +0300

    fix ignoring autoformat

--- .config/schnizvim/lua/plugins/formatter.lua
+++ .config/schnizvim/lua/plugins/formatter.lua
@@ -13,27 +13,31 @@ local formatters = {
   terraform = { "terraform_fmt" },
 }
 
+---@param bufnr buffer_nr
+local function is_ignored_from_autosave(bufnr)
+  local filename = vim.api.nvim_buf_get_name(bufnr)
+  local basename = vim.fs.basename(filename)
+  local dirname = vim.fs.dirname(filename)
+
+  if string.match(basename, "%.lua$") and string.match(dirname, "/vercel/proxy/") then
+    return true
+  end
+
+  return false
+end
+
 return {
   'stevearc/conform.nvim',
   ---@type conform.setupOpts
   opts = {
     lsp_fallback = true,
     formatters_by_ft = formatters,
-    formatters = {
-      [formatters.lua[1]] = {
-        ---@diagnostic disable-next-line: unused-local
-        condition = function(self, ctx)
-          if string.match(ctx.dirname, "/vercel/proxy/") then
-            return false
-          end
-
-          return true
-        end
-      }
-    },
     format_after_save = function(bufnr)
       -- Disable with a global or buffer-local variable
       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      if is_ignored_from_autosave(bufnr) then
         return
       end
       return { timeout_ms = 1000, lsp_fallback = true, async = true, formatters_by_ft = formatters }
