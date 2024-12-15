c9c4c71a83ccb0cbbb09b3f80792ed4ca94e2d0e
Gal Schlezinger <gal@spitfire.co.il>
2024-12-15 11:32:05 +0200

    fix lsp

--- .config/schnizvim/lua/lsp/custom.lua
+++ .config/schnizvim/lua/lsp/custom.lua
@@ -1,15 +1,11 @@
+---@type table<string,lspconfig.Config>
 return {
-  vercel_lsp = {
-    filetypes = { "json", "jsonc" },
-    command = { "/Users/schniz/Code/vercel/vercel_lsp/target/debug/vercel_lsp" }
-  },
+	sourcekit_lsp = {
+		filetypes = { "swift" },
+		cmd = { "xcrun", "sourcekit-lsp" }
+	},
 
-  sourcekit_lsp = {
-    filetypes = { "swift" },
-    command = { "xcrun", "sourcekit-lsp" }
-  },
-
-  -- git_lsp = {
-  --   command = { "/Users/schniz/Code/git_lsp/target/debug/git_lsp" },
-  -- }
+	-- git_lsp = {
+	--   command = { "/Users/schniz/Code/git_lsp/target/debug/git_lsp" },
+	-- }
 }
