030a608287fb75f8b1ecea7c964968322cc7e3e6
Gal Schlezinger <gal@spitfire.co.il>
2024-12-16 18:33:21 +0200

    Add snacks.lua

---
+++ .config/schnizvim/lua/plugins/snacks.lua
@@ -0,0 +1,18 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = { enabled = false },
+		indent = { enabled = true },
+		input = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		scope = { enabled = true },
+		scroll = { enabled = true },
+		statuscolumn = { enabled = true },
+		words = { enabled = true },
+	},
+}
