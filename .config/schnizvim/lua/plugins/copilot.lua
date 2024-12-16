f05bff68bf5674a25559c6f2a58ddfedd82398d9
Gal Schlezinger <gal@spitfire.co.il>
2024-12-16 18:39:04 +0200

    rewrite stuff

--- .config/schnizvim/lua/plugins/copilot.lua
+++ .config/schnizvim/lua/plugins/copilot.lua
@@ -1,29 +1,18 @@
--- return {
---   "github/copilot.vim",
---   -- cmd = { "Copilot" },
---   -- event = { "BufReadPost" },
---   init = function()
---     vim.g.copilot_filetypes = {
---       markdown = true,
---       yaml = true,
---     }
---   end
--- }
 return {
-  "zbirenbaum/copilot.lua",
-  dependencies = {
-    "zbirenbaum/copilot-cmp"
-  },
-  build = function()
-    vim.cmd("Copilot auth")
-  end,
-  opts = {
-    filetypes = {
-      ["*"] = true
-    }
-  },
-  config = function(_, opts)
-    require("copilot").setup(opts)
-    require("copilot_cmp").setup()
-  end
+	"zbirenbaum/copilot.lua",
+	dependencies = {
+		"zbirenbaum/copilot-cmp"
+	},
+	build = function()
+		vim.cmd("Copilot auth")
+	end,
+	opts = {
+		filetypes = {
+			["*"] = true
+		}
+	},
+	config = function(_, opts)
+		require("copilot").setup(opts)
+		require("copilot_cmp").setup()
+	end
 }
--- .config/schnizvim/lua/plugins/snacks.lua
+++ .config/schnizvim/lua/plugins/snacks.lua
@@ -11,7 +11,6 @@ return {
 		notifier = { enabled = true },
 		quickfile = { enabled = true },
 		scope = { enabled = true },
-		scroll = { enabled = true },
 		statuscolumn = { enabled = true },
 		words = { enabled = true },
 	},
