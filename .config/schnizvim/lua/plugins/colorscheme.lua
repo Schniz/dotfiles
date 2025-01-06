3b1dd0fca5a4e33319cb2c952ac1c2f41912b9d6
Gal Schlezinger <gal@spitfire.co.il>
2025-01-06 13:46:41 +0200

    make vim background transparent

--- .config/schnizvim/lua/plugins/colorscheme.lua
+++ .config/schnizvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-  "RRethy/nvim-base16",
-  config = function()
-    vim.cmd.colorscheme("base16-eighties")
-    vim.cmd [[highlight NonText guifg=#444444]]
-    -- vim.cmd [[highlight Normal guibg=NONE]]
-    -- vim.cmd [[highlight LineNr guibg=None]]
-    -- vim.cmd [[highlight SignColumn guibg=None]]
-  end
+	"RRethy/nvim-base16",
+	init = function()
+		vim.cmd.colorscheme("base16-eighties")
+		vim.cmd [[highlight NonText guifg=#444444]]
+		vim.cmd [[highlight Normal guibg=None]]
+		-- vim.cmd [[highlight LineNr guibg=None]]
+		-- vim.cmd [[highlight SignColumn guibg=None]]
+	end
 }
--- .config/schnizvim/lua/plugins/lsp.lua
+++ .config/schnizvim/lua/plugins/lsp.lua
@@ -72,6 +72,11 @@ return {
 		{
 			"j-hui/fidget.nvim",
 			opts = {
+				notification = {
+					window = {
+						winblend = 0,
+					},
+				},
 				progress = {
 					ignore = { "null-ls" },
 				},
