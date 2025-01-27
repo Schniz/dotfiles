07dfa7530fd4671be6a9b47c14479294e0d62a6a
Gal Schlezinger <gal@spitfire.co.il>
2025-01-27 21:42:47 +0200

    use vtsls instead of ts_ls

--- .config/schnizvim/lua/lsp/servers.lua
+++ .config/schnizvim/lua/lsp/servers.lua
@@ -1,16 +1,16 @@
 local servers = {
-	ts_ls = {
-		init_options = {
-			preferences = {
-				includeInlayEnumMemberValueHints = true,
-				includeInlayFunctionLikeReturnTypeHints = true,
-				includeInlayFunctionParameterTypeHints = true,
-				-- includeInlayParameterNameHints = 'all',
-				-- includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-				includeInlayPropertyDeclarationTypeHints = true,
-				includeInlayVariableTypeHints = true,
-			},
-		},
+	vtsls = {
+		-- init_options = {
+		-- 	preferences = {
+		-- 		includeInlayEnumMemberValueHints = true,
+		-- 		includeInlayFunctionLikeReturnTypeHints = true,
+		-- 		includeInlayFunctionParameterTypeHints = true,
+		-- 		-- includeInlayParameterNameHints = 'all',
+		-- 		-- includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+		-- 		includeInlayPropertyDeclarationTypeHints = true,
+		-- 		includeInlayVariableTypeHints = true,
+		-- 	},
+		-- },
 		on_attach = function(client, bufnr)
 			require("twoslash-queries").attach(client, bufnr)
 		end,
