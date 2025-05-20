988099d56d3375b514b167797fece1fa34751d5f
Gal Schlezinger <gal@spitfire.co.il>
2025-05-20 10:28:34 +0300

    make ]c [c silent

--- .config/schnizvim/lua/plugins/git.lua
+++ .config/schnizvim/lua/plugins/git.lua
@@ -1,4 +1,7 @@
 return {
   "tpope/vim-fugitive",
-  dependencies = { "tpope/vim-rhubarb", "airblade/vim-gitgutter" }
+  dependencies = { "tpope/vim-rhubarb", "airblade/vim-gitgutter" },
+  init = function()
+    vim.g.gitgutter_show_msg_on_hunk_jumping = 0
+  end,
 }
