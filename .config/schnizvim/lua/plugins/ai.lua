8a626ea30445f30acf7404614ea04c5596658037
Gal Schlezinger <gal@spitfire.co.il>
2025-05-21 09:20:16 +0300

    config codecompanion to be <leader>oa (open ai), h/t @dmmulroy

--- .config/schnizvim/lua/plugins/ai.lua
+++ .config/schnizvim/lua/plugins/ai.lua
@@ -1,8 +1,40 @@
+---@type LazySpec
 return {
   "olimorris/codecompanion.nvim",
-  opts = {},
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
+  },
+  keys = {
+    {
+      "<leader>oa",
+      "<cmd>CodeCompanionChat Toggle<cr>",
+      mode = "n",
+      desc = "Toggle Code Companion chat",
+      noremap = true,
+      silent = true,
+    },
+    {
+      "<leader>oa",
+      ":CodeCompanion<cr>",
+      mode = "v",
+      desc = "Prompt Code Companion on the current selection",
+      noremap = true,
+      silent = true,
+    },
+  },
+  opts = {
+    strategies = {
+      chat = {
+        keymaps = {
+          send = {
+            modes = { i = "<C-;>" },
+          },
+          close = {
+            modes = { n = "<C-s>", i = "<C-s>" },
+          },
+        },
+      },
+    },
   },
 }
